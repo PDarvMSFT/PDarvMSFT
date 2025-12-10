6d2f2766e107f2b51fe46ec34542c22ca23454f4
Philippe Darveau <philippe.darveau@hotmail.com>
2025-12-10 13:23:03 -0500

    modif

--- HAOS/homeassistant.sh
+++ HAOS/homeassistant.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/main/misc/build.func)
+source <(curl -s https://github.com/PDarvMSFT/PDarvMSFT/blob/e253817e4f9bf020290c16bd19f31c19c0ffb315/HAOS/build.func)
 # Copyright (c) 2021-2024 tteck
 # Author: tteck (tteckster)
 # License: MIT
